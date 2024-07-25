115e0189ff7c97ab1d39b60be6a0897c902c1e02
KaberaSamuel <kaberanshutis@gmail.com>
2024-07-25 11:44:30 +0200

    refactoring and adding comments for explaining the code

--- script.rb
+++ script.rb
@@ -1,22 +1,21 @@
+#Creating function that will sort an array
 def bubble_sort(array)
   while !array.is_sorted?
     array.each_with_index do |item,index|
       next if index == 0
-      if item < array[index - 1]
-        before = array[index - 1]
-        array[index - 1] = item
-        array[index] = before 
-      end
+      array[index - 1], array[index] = array[index], array[index - 1] if item < array[index - 1]        
     end
   end
   array
 end
 
+# Creating method to check if an array is sorted or not 
 class Array
   def is_sorted?
     current = self[0]
     for item in self 
       return false if item < current
+      current = item
     end
     return true
   end
